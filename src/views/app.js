62f50d4ae30eff37da598f9b6a1536e973b416d6
bcronin2 <bcronin2@gmail.com>
2018-06-25 21:13:37 -0700

    minor cleanup

--- src/views/app.js
+++ src/views/app.js
@@ -3,14 +3,9 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-
     this.videos = new Videos(exampleVideoData);
-
     this.videoPlayerView = new VideoPlayerView({ collection: this.videos });
-
     this.videoListView = new VideoListView({ collection: this.videos });
-
-
     this.render();
   },
 
